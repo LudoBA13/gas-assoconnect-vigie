6f23b17f95c727347f759368a18ae16078b152dd
LudoBA13 <ba130.ludovic.arnaud@banquealimentaire.org>
2026-07-11 20:45:39 +0200

    feat: add Setup.js with formatting

---
+++ src/Setup.js
@@ -0,0 +1,32 @@
+const alerts = require('./alerts');
+
+function setUpSheets()
+{
+	createSheets();
+}
+
+function createSheets()
+{
+	const ss = SpreadsheetApp.getActiveSpreadsheet();
+
+	alerts.forEach(alert =>
+	{
+		const sheetName = 'Alert-' + alert.name;
+		let sheet = ss.getSheetByName(sheetName);
+
+		if (!sheet)
+		{
+			sheet = ss.insertSheet(sheetName);
+		}
+
+		const a1Cell = sheet.getRange('A1');
+		const currentFormula = a1Cell.getFormula();
+
+		if (currentFormula !== alert.formula)
+		{
+			a1Cell.setFormula(alert.formula);
+		}
+	});
+}
+
+module.exports = { setUpSheets };
