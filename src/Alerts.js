e1188cc18cc73e0d1dc47b1d356db1f51152ef05
LudoBA13 <ba130.ludovic.arnaud@banquealimentaire.org>
2026-07-19 20:41:19 +0200

    Update CNESSansÉpicerie alert description

--- src/Alerts.js
+++ src/Alerts.js
@@ -5,7 +5,7 @@ function getAlerts()
 		"name": "CNESSansÉpicerie",
 		"sheetName": "Alert-CNESSansÉpicerie",
 		"type": "issue",
-		"description": "Partenaires qui reçoivent des produits CNES de la BA mais ne sont pas type épicerie sociale.",
+		"description": "Partenaires qui reçoivent des produits CNES de la BA mais ne sont pas de type épicerie sociale.",
 		"message": "Reçoit des produits CNES mais n'est pas une épicerie sociale.",
 		"formula": "=LET(\n  dbHeaders; ACStructures!$1:$1;\n  dbRows;    ACStructures!$2:$1000;\n\n  outputHeaders; { \"ID du Contact\" \\ \"Nom\" };\n  outputIndexes; BYCOL(outputHeaders; LAMBDA(colName; MATCH(colName; dbHeaders; 0)));\n\n  typeCol;  INDEX(dbRows; 0; MATCH(\"Type de structure\"; dbHeaders; 0));\n  cnesCol;  INDEX(dbRows; 0; MATCH(\"Le partenaire reçoit-il des produits CNES ?\"; dbHeaders; 0));\n  modesCol; INDEX(dbRows; 0; MATCH(\"Modes de distribution de l'aide alimentaire\"; dbHeaders; 0));\n\n  outputRows; FILTER(\n    CHOOSECOLS(dbRows; outputIndexes);\n    ISNUMBER(FIND(\"1_Partenaire\"; typeCol));\n    ISERR(FIND(\"Epicerie Sociale\"; modesCol));\n    cnesCol = \"Oui de la BA\"\n  );\n\n  VSTACK(outputHeaders; outputRows)\n)\n"
 	},
